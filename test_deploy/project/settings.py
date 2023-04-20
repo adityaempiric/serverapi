538f69d24fa91f3091eb9a063c5f3bfa9381ceba
adityaempiric <ad.empiricinfotech@gmail.com>
2023-04-20 12:11:19 +0530

    13th commit

--- test_deploy/project/settings.py
+++ test_deploy/project/settings.py
@@ -85,10 +85,11 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER':'postgres',
-        'HOST':'db',
-        'PORT':5432,
+        'NAME': 'FinalShree2',
+        'USER': 'postgres',
+        'PASSWORD' : '0000',
+        'HOST' : '137.184.158.107',
+        'PORT' : '5432',
     }
 }
 
